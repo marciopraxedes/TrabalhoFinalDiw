266b69182c158a9611eb94a2edb4a4d3f46ff72f
Márcio Praxedes <marcio.praxedes@hotmail.com.br>
2018-11-19 22:39:29 -0200

    Inclusao da imagem

--- js/dados.js
+++ js/dados.js
@@ -11,7 +11,8 @@ var dbVeiculos = {
         "ano": "2014",
         "combustivel": "gas/alcool",
         "potencia": "80cv",
-        "fipe": "R$ 34 000,00"        
+        "fipe": "R$ 34 000,00",
+        "imagem": "Sandero.jpg"       
         },
 
         {  
@@ -22,7 +23,8 @@ var dbVeiculos = {
         "ano": "2014",
         "combustivel": "gas/alcool",
         "potencia": "80cv",
-        "fipe": "R$ 34 000,00"        
+        "fipe": "R$ 34 000,00",
+        "imagem": "teste"         
         },
 ]
 }
@@ -49,7 +51,8 @@ var dbVeiculos = {
                     "ano": veiculo.ano,
                     "combustivel": veiculo.combustivel,
                     "potencia": veiculo.potencia,
-                    "fipe": veiculo.fipe
+                    "fipe": veiculo.fipe,
+                    "imagem": veiculo.imagem
                 };
         // Insere o novo objeto no array
         db.registros.push(novoVeiculo);
@@ -64,13 +67,15 @@ var dbVeiculos = {
                 let index = db.registros.map(obj => obj.id).indexOf(id);
             
                 // Altera os dados do objeto no array
+                
                 db.registros[index].veiculo = veiculo.veiculo,
                 db.registros[index].versao = veiculo.versao,
                 db.registros[index].montadora = veiculo.montadora,
                 db.registros[index].ano = veiculo.ano,
                 db.registros[index].combustivel = veiculo.combustivel,
                 db.registros[index].potencia = veiculo.potencia,
-                db.registros[index].fipe = veiculo.fipe               
+                db.registros[index].fipe = veiculo.fipe,   
+                db.registros[index].imagem = veiculo.imagem            
             
                 displayMessage("Veiculo alterado com sucesso");
             
